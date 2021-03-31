da046bc3b003d9fe716c6305e59cb5c6f05cb24d
Bartłomiej Półtorak <poltorakbartlomiej@gmail.com>
2021-04-01 00:20:18 +0200

    Few changes in header

--- src/pages/pl.js
+++ src/pages/pl.js
@@ -9,7 +9,8 @@ const plIndexPage = () => {
         <title>Polka Translations - Tłumaczenia pisemne i ustne w języku polskim i duńskim</title>
         <meta property="og:title" content="Polka Translations" />
         <meta property="og:description" content="Tłumaczenia pisemne i ustne w języku polskim i duńskim" />
-        <meta property="og:url" content="https://res.cloudinary.com/bpweb/image/upload/v1617228731/ogimagepl_twnbox.png" />
+        <meta property="og:url" content="https://polkatranslations.dk/pl" />
+        <meta property="og:image" content="https://res.cloudinary.com/bpweb/image/upload/v1617228731/ogimagepl_twnbox.png" />
       </Helmet>
       <Layout lang="pl"/>
     </>
